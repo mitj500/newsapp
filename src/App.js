383bd528ce63fa69c5d8fb9a05ba307f9362f369
mitj500 <mitjain9987900@gmail.com>
2024-07-11 23:48:17 +0530

    firast

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Navbar from './components/Navbar' // Import from the correct location
+import Navbar from '../components/Navbar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar/>
       </div>
     )
   }
