1111f76882078e686df6165486ed613a88c4bbc7
mitj500 <mitjain9987900@gmail.com>
2024-08-04 22:54:14 +0530

    further updates on changing all the components to the function based

--- src/components/News.js
+++ src/components/News.js
@@ -1,21 +1,18 @@
-import React, { Component } from "react";
+import React, { useEffect ,useState} from "react";
+
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
-
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
+const News = (props)=> {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
+  // document.title = `${this.capitalizeFirstLetter(
+  //   props.category
+  // )} - NewsMonkey`;
 
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -30,9 +27,7 @@ export class News extends Component {
       error: null,
       totalResults: 0,
     };
-    document.title = `${this.capitalizeFirstLetter(
-      this.props.category
-    )} - NewsMonkey`;
+    
   }
 
   sleep = (ms) => {
@@ -43,8 +38,9 @@ export class News extends Component {
     this.updateNews(); // Fetch initial articles when component mounts
   }
 
-  async updateNews(retries = 3, delay = 1000) {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1a5fa164f12d4375a3d64fbc7ce952f7&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const updateNews= async(retries = 3, delay = 1000)=> {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${this.state.page}&pageSize=${props.pageSize}`;
     this.setState({ loading: true, error: null });
     try {
       let data = await fetch(url);
@@ -56,7 +52,10 @@ export class News extends Component {
         }
         throw new Error(`Error: ${data.status}`);
       }
+      props.setProgress(30);
       let parsedData = await data.json();
+      props.setProgress(70);
+      setArticles(parsedData.articles);
       this.setState({
         articles: parsedData.articles || [], // Ensure articles is always an array
         totalResults: parsedData.totalResults,
@@ -66,12 +65,14 @@ export class News extends Component {
     } catch (error) {
       console.error("Failed to fetch articles", error);
       this.setState({ loading: false, error: error.message });
+      
     }
+    props.setProgress(100);
   }
 
   fetchMoreData = async (retries = 3, delay = 1000) => {
     this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1a5fa164f12d4375a3d64fbc7ce952f7&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${this.state.page + 1}&pageSize=${props.pageSize}`;
     // this.setState({ loading: true, error: null });
     try {
       let data = await fetch(url);
@@ -102,7 +103,7 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h1 className="text-center" style={{ margin: "35px 0px" }}>
-          NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+          NewsMonkey - Top {this.capitalizeFirstLetter(props.category)}{" "}
           Headlines
         </h1>
         {this.state.loading && <Spinner/>}
@@ -141,4 +142,174 @@ export class News extends Component {
   }
 }
 
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 export default News;
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import NewsItem from "./NewsItem";
+// import Spinner from "./Spinner";
+// import PropTypes from "prop-types";
+// import InfiniteScroll from "react-infinite-scroll-component";
+
+// export class News extends Component {
+//   static defaultProps = {
+//     country: "in",
+//     pageSize: 8,
+//     category: "general",
+//   };
+
+//   static propTypes = {
+//     country: PropTypes.string,
+//     pageSize: PropTypes.number,
+//     category: PropTypes.string,
+//   };
+
+//   capitalizeFirstLetter = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       articles: [],
+//       loading: true,
+//       page: 1,
+//       error: null,
+//       totalResults: 0,
+//     };
+//     document.title = `${this.capitalizeFirstLetter(
+//       props.category
+//     )} - NewsMonkey`;
+//   }
+
+//   sleep = (ms) => {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//   };
+
+//   async componentDidMount() {
+//     this.updateNews(); // Fetch initial articles when component mounts
+//   }
+
+//   async updateNews(retries = 3, delay = 1000) {
+//     props.setProgress(10);
+//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${this.state.page}&pageSize=${props.pageSize}`;
+//     this.setState({ loading: true, error: null });
+//     try {
+//       let data = await fetch(url);
+//       if (!data.ok) {
+//         if (data.status === 429 && retries > 0) {
+//           console.log(`Retrying... attempts left: ${retries}`);
+//           await this.sleep(delay);
+//           return this.updateNews(retries - 1, delay * 2); // Exponential backoff
+//         }
+//         throw new Error(`Error: ${data.status}`);
+//       }
+//       let parsedData = await data.json();
+//       props.setProgress(30);
+//       this.setState({
+//         articles: parsedData.articles || [], // Ensure articles is always an array
+//         totalResults: parsedData.totalResults,
+//         loading: false,
+//       });
+//       console.log(parsedData); // Check the response data
+//     } catch (error) {
+//       console.error("Failed to fetch articles", error);
+//       this.setState({ loading: false, error: error.message });
+      
+//     }
+//     props.setProgress(100);
+//   }
+
+//   fetchMoreData = async (retries = 3, delay = 1000) => {
+//     this.setState({ page: this.state.page + 1 });
+//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${this.state.page + 1}&pageSize=${props.pageSize}`;
+//     // this.setState({ loading: true, error: null });
+//     try {
+//       let data = await fetch(url);
+//       if (!data.ok) {
+//         if (data.status === 429 && retries > 0) {
+//           console.log(`Retrying... attempts left: ${retries}`);
+//           await this.sleep(delay);
+//           return this.updateNews(retries - 1, delay * 2); // Exponential backoff
+//         }
+//         throw new Error(`Error: ${data.status}`);
+//       }
+//       let parsedData = await data.json();
+//       this.setState({
+//         articles: this.state.articles.concat(parsedData.articles), // Correct the typo
+//         totalResults: parsedData.totalResults,
+//         loading: false,
+//       });
+//       console.log(parsedData); // Check the response data
+//     } catch (error) {
+//       console.error("Failed to fetch articles", error);
+//       this.setState({ loading: false, error: error.message });
+//     }
+//   };
+
+//   render() {
+//     const { articles, loading, error } = this.state;
+
+//     return (
+//       <div className="container my-3">
+//         <h1 className="text-center" style={{ margin: "35px 0px" }}>
+//           NewsMonkey - Top {this.capitalizeFirstLetter(props.category)}{" "}
+//           Headlines
+//         </h1>
+//         {this.state.loading && <Spinner/>}
+//         <InfiniteScroll
+//           dataLength={this.state.articles.length}
+//           next={this.fetchMoreData}
+//           hasMore={this.state.articles.length !== this.state.totalResults} // Correctly reference totalResults
+//           loader={<Spinner />}
+//         ><div className="container">
+//           <div className="container">
+//             {error && <div>Error: {error}</div>}
+//             <div className="row">
+//               {!loading && articles && articles.length > 0
+//                 ? articles.map((element) => (
+//                     <div className="col-md-4" key={element.url}>
+//                       <NewsItem
+//                         title={element.title ? element.title : ""}
+//                         description={
+//                           element.description ? element.description : ""
+//                         }
+//                         imageUrl={element.urlToImage}
+//                         newsUrl={element.url}
+//                         author={element.author}
+//                         date={element.publishedAt}
+//                         source={element.source.name}
+//                       />
+//                     </div>
+//                   ))
+//                 : !loading && !error && <div>No articles found</div>}
+//             </div>
+//           </div>
+//           </div>
+//         </InfiniteScroll>
+//       </div>
+//     );
+//   }
+// }
+
+// export default News;
--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
-import loading from './loading.gif';
-export default class spinner extends Component {
-  render() {
-    return (
-      <div className='text-center'>
-        <img   className="my-3" src={loading} alt='load'/>
-      </div>
-    )
-  }
-}
+import React from "react";
+import loading from "./loading.gif";
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img className="my-3" src={loading} alt="load" />
+    </div>
+  );
+};
+export default Spinner;
+
+//import React, { Component } from 'react'
+// import loading from './loading.gif';
+// export default class spinner extends Component {
+//   render() {
+//     return (
+//       <div className='text-center'>
+//         <img   className="my-3" src={loading} alt='load'/>
+//       </div>
+//     )
+//   }
+// }
